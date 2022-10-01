f9f6e057fada969089657e22813077d89350b724
Shintaro <mori@hey.com>
2022-10-01 11:24:03 +0900

    add Recoil to share todo data

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,29 +1,38 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import {
-  collection,
-  doc,
-  getDocs,
-  gettDoc,
-  onSnapshot,
-  querySnapshot,
-} from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
+import { useRecoilState } from "recoil";
+import { todoState } from "../../components/atoms";
+import { useRouter } from "next/router";
 
 export default function Todos() {
-  const [todos, setTodos] = useState("");
+  const [todos, setTodos] = useState([{ title: "Loading ...", id: "initial" }]);
 
+  const [statedTodo, setStatedTodo] = useRecoilState(todoState);
+
+  const router = useRouter();
+
+  // firestoreの"todos" collectionの、各ドキュメントを読み込む
   useEffect(() => {
-    const todoCollectionRef = collection(db, "todos");
-    getDocs(todoCollectionRef).then((querySnapshot) => {
-      console.log(querySnapshot);
+    // コレクション"todos"から、各ドキュメント（各todo）を展開
+    const unsub = onSnapshot(collection(db, "todos"), (snapshot) => {
+      // todosの配列にセット。ドキュメントのid番号を割り振り
+      setTodos(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     });
-    // if (todoSnap.exists()) {
-    //   console.log(todoSnap.data());
-    // } else {
-    //   console.log("no such document!");
-    // }
+    return unsub;
   }, []);
+
+  // 詳細ボタンをクリックした時の関数
+  const handleDetailButtonClick = (todo) => {
+    // set
+    setStatedTodo({
+      title: todo.title,
+      detail: todo.detail,
+    });
+
+    router.push("/todos/detail");
+  };
 
   return (
     <div className="m-4">
@@ -42,42 +51,36 @@ export default function Todos() {
       {/* todo field */}
       <div>
         <ul>
-          <li className="flex bg-black justify-between items-center p-2 mb-4 w-[70%]">
-            <span>テスト入力２</span>
-            <div className="flex items-center">
-              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[110px] h-[30px] p-1 mr-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="waiting">Waiting</option>
-                <option value="working">Working</option>
-                <option value="completed">Completed</option>
-              </select>
-              <Link href="/todos/detail">
+          {/* firebaseに格納されているtodosを展開 */}
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex bg-black justify-between items-center p-2 mb-4 w-[70%]"
+            >
+              <span>{todo.title}</span>
+              <div className="flex items-center">
+                {/* ステータスのセレクタ */}
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[110px] h-[30px] p-1 mr-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="waiting">Waiting</option>
+                  <option value="working">Working</option>
+                  <option value="completed">Completed</option>
+                </select>
+
+                {/* 詳細ボタン */}
+                {/* <Link href="/todos/detail"> */}
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                  onClick={() => {
+                    handleDetailButtonClick(todo);
+                  }}
                 >
                   詳細
                 </button>
-              </Link>
-            </div>
-          </li>
-          <li className="flex bg-black justify-between items-center p-2 w-[70%]">
-            <span>テスト入力２</span>
-            <div className="flex items-center">
-              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[110px] h-[30px] p-1 mr-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="waiting">Waiting</option>
-                <option value="working">Working</option>
-                <option value="completed">Completed</option>
-              </select>
-              <Link href="/todos/detail">
-                <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >
-                  詳細
-                </button>
-              </Link>
-            </div>
-          </li>
+                {/* </Link> */}
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
