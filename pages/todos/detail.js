7b55410de28ff64ae2dfacdc2bc92fe53ae6d21b
Shintaro <mori@hey.com>
2022-10-02 10:02:17 +0900

    add delete function

--- pages/todos/detail.js
+++ pages/todos/detail.js
@@ -1,10 +1,24 @@
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 import Link from "next/link";
 import { useRecoilValue } from "recoil";
 import { todoState } from "../../components/atoms";
+import { useRouter } from "next/router";
 
 export default function Detail() {
   // atomsより、todoの値を取得
   const statedTodo = useRecoilValue(todoState);
+  const router = useRouter();
+
+  // 削除ボタンをクリックした時の関数
+  const handleDelete = async (id) => {
+    // ドキュメントのを、Recoilで日っぱてきているstatedTodoのidで参照
+    const docRef = doc(db, "todos", id); //第３引数は、document id
+    // document（対象のTODO）を削除
+    await deleteDoc(docRef);
+    // /todosへ遷移
+    router.push("/todos");
+  };
 
   return (
     <div className="p-6">
@@ -12,18 +26,24 @@ export default function Detail() {
       {/* input field */}
       <div>
         <h1>タイトル：{statedTodo.title}</h1>
-        <h2>詳細　　：{statedTodo.detail}</h2>
-        <p>id : {statedTodo.id}</p>
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          編集
-        </button>
+        <h2>詳細：{statedTodo.detail}</h2>
+        <p>Doc id : {statedTodo.id}</p>
 
+        {/* 編集ボタン */}
+        <Link href="/todos/edit">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            編集
+          </button>
+        </Link>
+
+        {/* 削除ボタン */}
         <button
           type="button"
           className="px-5 py-1.5 mb-2 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          onClick={() => handleDelete(statedTodo.id)}
         >
           削除
         </button>
@@ -33,7 +53,7 @@ export default function Detail() {
             type="button"
             className="px-5 py-1.5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
-            取消
+            戻る
           </button>
         </Link>
       </div>
