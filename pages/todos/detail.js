44f44a0201953ccd4721c15250a5e7fff602ab66
Shintaro <mori@hey.com>
2022-10-02 09:14:16 +0900

    change the padding

--- pages/todos/detail.js
+++ pages/todos/detail.js
@@ -7,13 +7,13 @@ export default function Detail() {
   const statedTodo = useRecoilValue(todoState);
 
   return (
-    <div className="p-4">
+    <div className="p-6">
       <h1 className="text-white text-xl">詳細ページ</h1>
-      <hr />
       {/* input field */}
       <div>
-        <h1>{statedTodo.title}</h1>
-        <h2>{statedTodo.detail}</h2>
+        <h1>タイトル：{statedTodo.title}</h1>
+        <h2>詳細　　：{statedTodo.detail}</h2>
+        <p>id : {statedTodo.id}</p>
         <button
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
