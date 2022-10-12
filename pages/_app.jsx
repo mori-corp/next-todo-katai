07ac21dc300d67cd8c052803e537640accc21d8d
Shintaro <mori@hey.com>
2022-10-12 09:16:06 +0900

    uninstall next-auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,11 @@
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </SessionProvider>
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
   return (
     <div>
       <Head>
@@ -21,9 +11,6 @@ export default function Home() {
         <link href="/dist/output.css" rel="stylesheet"></link>
       </Head>
       <div>
-        <p>Hello</p>
-        Not signed in <br />
-        <button onClick={() => signIn("google")}>Sign in</button>
         <h1>認証ページ（未実装）</h1>
         <Link href="/todos">
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1.5 mt-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
